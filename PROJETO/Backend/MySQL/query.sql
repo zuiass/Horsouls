29b9828fa26bf3fa60c1baf787ecae20fbd832c4
Josué <josuealexandre067@gmail.com>
2025-03-07 16:34:27 -0400

    Update query.sql

--- PROJETO/Backend/MySQL/query.sql
+++ PROJETO/Backend/MySQL/query.sql
@@ -3,11 +3,11 @@ USE hoursouls;
 
 CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    tipo
+    tipo VARCHAR(6) DEFAULT 'Padrão',
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     senha VARCHAR(255) NOT NULL,
     criado TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-drop  table usuarios;
+SELECT * FROM usuarios;
